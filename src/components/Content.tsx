f191354c6a91d2e14f491adf3347f8e2a4d3efe2
Niels-Erik Nandal <nielseriknandal@gmail.com>
2025-09-01 14:50:08 -0400

    form submit actions

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,30 @@
 export default function Content() {
+
+    const ingredients = ["Chicken", "Beef", "Pork", "Fish", "Eggs", "Milk", "Cheese", "Butter", "Bread", "Rice", "Pasta", "Tomato", "Potato", "Carrot", "Onion", "Garlic", "Pepper", "Salt", "Pepper"];
+
+    const ingredientElements = ingredients.map((ingredient) => {
+        return <li>{ingredient}</li>
+    })
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log("Submit");
+        const formData = new FormData(e.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        console.log(newIngredient)
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient" />
-                <button type="submit">Add ingredient</button>
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+                <input type="text" name="ingredient" placeholder="e.g. oregano" aria-label="Add ingredient" />
+                <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientElements}
+            </ul>
         </main>
     )
 }
