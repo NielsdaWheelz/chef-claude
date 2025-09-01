53b008a1b82106e0e12155d49a9a94d43b90e714
Niels-Erik Nandal <nielseriknandal@gmail.com>
2025-09-01 13:54:58 -0400

    header, form beginnings

---
+++ src/components/Content.tsx
@@ -0,0 +1,10 @@
+export default function Content() {
+    return (
+        <main className="main">
+            <form className="add-ingredient-form">
+                <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient" />
+                <button>Add ingredient</button>
+            </form>
+        </main>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import viteLogo from '/vite.svg'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src={viteLogo} alt="logo" />
+            <h2>Chef Claude</h2>
+        </header>
+    )
+}
