73c2b29da132da74cd663c510c5f5369dac5c495
Niels-Erik Nandal <nielseriknandal@gmail.com>
2025-09-04 22:51:22 -0400

    markdown

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,8 +1,10 @@
+import ReactMarkdown from 'react-markdown'
+
 export default function Recipe(props: { recipe: string }) {
     return (
-        <>
+        <section className="suggested-recipe-container" aria-label="Suggested recipe">
             <h2>Recipe</h2>
-            <>{ props.recipe }</>
-        </>
+            <ReactMarkdown>{ props.recipe }</ReactMarkdown>
+        </section>
     )
 }
